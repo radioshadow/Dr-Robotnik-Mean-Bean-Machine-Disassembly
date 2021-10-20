054b9d7116c57daed07f2f24ff3cf506058eb0e7
RadioTails <62575054+radioshadow@users.noreply.github.com>
2021-10-21 00:17:06 +0100

    Add files via upload

---
+++ data/palettes/palette table.asm
@@ -0,0 +1,273 @@
+; --------------------------------------------------------------
+; Palette table
+; --------------------------------------------------------------
+
+Pal_Black:
+	incbin	"data/palettes/palettes/0 - Black.bin"
+	
+Pal_RedYellowPuyos:
+	incbin	"data/palettes/palettes/1 - Red Yellow Puyos.bin"
+	
+Pal_BluePurplePuyos:
+	incbin	"data/palettes/palettes/2 - Blue Purple Puyos.bin"
+	
+Pal_GreenTealPuyos:
+	incbin	"data/palettes/palettes/3 - Green Teal Puyos.bin"
+	
+Pal_2856:
+	incbin	"data/palettes/palettes/4.bin"
+	
+Pal_2876:
+	incbin	"data/palettes/palettes/5.bin"
+	
+Pal_2896:
+	incbin	"data/palettes/palettes/6.bin"
+	
+Pal_28B6:
+	incbin	"data/palettes/palettes/7.bin"
+	
+Pal_28D6:
+	incbin	"data/palettes/palettes/8.bin"
+	
+Pal_28F6:
+	incbin	"data/palettes/palettes/9.bin"
+	
+Pal_GameOverFG:
+	incbin	"data/palettes/palettes/10 - Game Over FG.bin"
+	
+Pal_GameOverBG:
+	incbin	"data/palettes/palettes/11 - Game Over BG.bin"
+	
+Pal_White:
+	incbin	"data/palettes/palettes/12 - White 1.bin"
+	
+Pal_White2:
+	incbin	"data/palettes/palettes/13 - White 2.bin"
+	
+Pal_2996:
+	incbin	"data/palettes/palettes/14.bin"
+	
+Pal_29B6:
+	incbin	"data/palettes/palettes/15.bin"
+	
+Pal_29D6:
+	incbin	"data/palettes/palettes/16.bin"
+	
+Pal_29F6:
+	incbin	"data/palettes/palettes/17.bin"
+	
+Pal_HighScoresBG:
+	incbin	"data/palettes/palettes/18 - High Scores BG.bin"
+	
+Pal_2A36:
+	incbin	"data/palettes/palettes/19.bin"
+	
+Pal_2A56:
+	incbin	"data/palettes/palettes/20.bin"
+	
+Pal_Options:
+	incbin	"data/palettes/palettes/21 - Options.bin"
+	
+Pal_2A96:
+	incbin	"data/palettes/palettes/22.bin"
+	
+Pal_2AB6:
+	incbin	"data/palettes/palettes/23.bin"
+	
+Pal_2AD6:
+	incbin	"data/palettes/palettes/24.bin"
+	
+Pal_2AF6:
+	incbin	"data/palettes/palettes/25.bin"
+	
+Pal_2B16:
+	incbin	"data/palettes/palettes/26.bin"
+	
+Pal_OptTextBlue:
+	incbin	"data/palettes/palettes/27 - Options Text Blue.bin"
+	
+Pal_OptTextRed:
+	incbin	"data/palettes/palettes/28 - Options Text Red.bin"
+	
+Pal_OptTextGreen:
+	incbin	"data/palettes/palettes/29 - Options Text Green.bin"
+	
+Pal_TutorialJoystick:
+	incbin	"data/palettes/palettes/30 - Tutorial Joystick.bin"
+	
+Pal_2BB6:
+	incbin	"data/palettes/palettes/31.bin"
+	
+Pal_2BD6:
+	incbin	"data/palettes/palettes/32.bin"
+	
+Pal_2BF6:
+	incbin	"data/palettes/palettes/33.bin"
+	
+Pal_2C16:
+	incbin	"data/palettes/palettes/34.bin"
+	
+Pal_2C36:
+	incbin	"data/palettes/palettes/35.bin"
+	
+Pal_2C56:
+	incbin	"data/palettes/palettes/36.bin"
+	
+Pal_2C76:
+	incbin	"data/palettes/palettes/37.bin"
+	
+Pal_2C96:
+	incbin	"data/palettes/palettes/38.bin"
+	
+Pal_2CB6:
+	incbin	"data/palettes/palettes/39.bin"
+	
+Pal_2CD6:
+	incbin	"data/palettes/palettes/40.bin"
+	
+Pal_2CF6:
+	incbin	"data/palettes/palettes/41.bin"
+	
+Pal_Scratch:
+	incbin	"data/palettes/palettes/42 - Scratch.bin"
+	
+Pal_Frankly:
+	incbin	"data/palettes/palettes/43 - Frankly.bin"
+	
+Pal_Coconuts:
+	incbin	"data/palettes/palettes/44 - Coconuts.bin"
+	
+Pal_Dynamight:
+	incbin	"data/palettes/palettes/45 - Dynamight.bin"
+	
+Pal_Grounder:
+	incbin	"data/palettes/palettes/46 - Grounder.bin"
+	
+Pal_DavySprocket:
+	incbin	"data/palettes/palettes/47 - Davy Sprocket.bin"
+	
+Pal_Spike:
+	incbin	"data/palettes/palettes/48 - Spike.bin"
+	
+Pal_CoconutsIntro:
+	incbin	"data/palettes/palettes/49 - Coconuts Intro.bin"
+	
+Pal_LevelIntroFG:
+	incbin	"data/palettes/palettes/50 - Level Intro FG.bin"
+	
+Pal_LevelIntroBG:
+	incbin	"data/palettes/palettes/51 - Level Intro BG.bin"
+	
+Pal_RobotnikLair:
+	incbin	"data/palettes/palettes/52 - Robotnik Lair.bin"
+	
+Pal_GameIntroGrounder:
+	incbin	"data/palettes/palettes/53 - Game Intro Grounder.bin"
+	
+Pal_GameIntroRobotnik:
+	incbin	"data/palettes/palettes/54 - Game Intro Robotnik.bin"
+	
+Pal_GameIntroScratch:
+	incbin	"data/palettes/palettes/55 - Game Intro Scratch.bin"
+	
+Pal_MainMenu:
+	incbin	"data/palettes/palettes/56 - Main Menu.bin"
+	
+Pal_MainMenuSel:
+	incbin	"data/palettes/palettes/57 - Main Menu Select.bin"
+	
+Pal_GameOverText:
+	incbin	"data/palettes/palettes/58 - Game Over Text.bin"
+	
+Pal_MainMenuShadow:
+	incbin	"data/palettes/palettes/59 - Main Menu Shadow.bin"
+	
+Pal_DragonBreath:
+	incbin	"data/palettes/palettes/60 - Dragon Breath.bin"
+	
+Pal_FranklyIntro:
+	incbin	"data/palettes/palettes/61 - Frankly Intro.bin"
+	
+Pal_Humpty:
+	incbin	"data/palettes/palettes/62 - Humpty.bin"
+	
+Pal_Robotnik:
+	incbin	"data/palettes/palettes/63 - Robotnik.bin"
+	
+Pal_Skweel:
+	incbin	"data/palettes/palettes/64 - Skweel.bin"
+	
+Pal_DavySprocketIntro:
+	incbin	"data/palettes/palettes/65 - Davy Sprocket Intro.bin"
+	
+Pal_DynamightIntro:
+	incbin	"data/palettes/palettes/66 - Dynamight Intro.bin"
+	
+Pal_SirFfuzzyLogik:
+	incbin	"data/palettes/palettes/67 - Sir Ffuzzy-Logik.bin"
+	
+Pal_Arms:
+	incbin	"data/palettes/palettes/68 - Arms.bin"
+	
+Pal_ArmsIntro:
+	incbin	"data/palettes/palettes/69 - Arms Intro.bin"
+	
+Pal_3096:
+	incbin	"data/palettes/palettes/70.bin"
+	
+Pal_SpikeIntro:
+	incbin	"data/palettes/palettes/71 - Spike Intro.bin"
+	
+Pal_DragonBreathIntro:
+	incbin	"data/palettes/palettes/72 - Dragon Breath Intro.bin"
+	
+Pal_SirFfuzzyLogikIntro:
+	incbin	"data/palettes/palettes/73 - Sir Ffuzzy-Logik Intro.bin"
+	
+Pal_HumptyIntro:
+	incbin	"data/palettes/palettes/74 - Humpty Intro.bin"
+	
+Pal_GrounderIntro:
+	incbin	"data/palettes/palettes/75 - Grounder Intro.bin"
+	
+Pal_3156:
+	incbin	"data/palettes/palettes/76.bin"
+	
+Pal_SkweelIntro:
+	incbin	"data/palettes/palettes/77 - Skweel Intro.bin"
+	
+Pal_ScratchIntro:
+	incbin	"data/palettes/palettes/78 - Scratch Intro.bin"
+	
+Pal_Title1:
+	incbin	"data/palettes/palettes/79 - Title 1.bin"
+	
+Pal_Title2:
+	incbin	"data/palettes/palettes/80 - Title 2.bin"
+	
+Pal_Title3:
+	incbin	"data/palettes/palettes/81 - Title 3.bin"
+	
+Pal_3216:
+	incbin	"data/palettes/palettes/82.bin"
+	
+Pal_Password4:
+	incbin	"data/palettes/palettes/83 - Password 4.bin"
+	
+Pal_Password3:
+	incbin	"data/palettes/palettes/84 - Password 3.bin"
+	
+Pal_EndingBeans:
+	incbin	"data/palettes/palettes/85 - Ending Beans.bin"
+	
+Pal_SegaLogo:
+	incbin	"data/palettes/palettes/86 - Sega Logo.bin"
+	
+Pal_32B6:
+	incbin	"data/palettes/palettes/87.bin"
+	
+Pal_Title4:
+	incbin	"data/palettes/palettes/88 - Title 4.bin"
+	
+Pal_32F6:
+	incbin	"data/palettes/palettes/89.bin"
