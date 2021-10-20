cd931c0db604fbe45e61f94f3da73fe1d29116c4
RadioTails <62575054+radioshadow@users.noreply.github.com>
2021-10-21 00:09:56 +0100

    Add files via upload

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 @echo off
-compiler\asm68k /p /o ae- /o l. main.asm, Dr_Robotnik's_Mean_Bean_Machine.md, , Dr Robotnik's Mean Bean Machine.lst > log.txt
+assembler\asm68k /p /o ae- /o l. main.asm, Dr_Robotnik's_Mean_Bean_Machine.md, , Dr Robotnik's Mean Bean Machine.lst > log.txt
 type log.txt
-compiler\fixheadr Dr_Robotnik's_Mean_Bean_Machine.md
+assembler\fixheadr Dr_Robotnik's_Mean_Bean_Machine.md
