f6702d8fbe14c8240acde6095cc6e6ebb442b722
RadioTails <62575054+radioshadow@users.noreply.github.com>
2021-12-24 22:49:38 +0000

    Delete make.bat

--- make.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-assembler\asm68k /p /o ae- /o l. main.asm, Dr_Robotnik's_Mean_Bean_Machine.md, , Dr Robotnik's Mean Bean Machine.lst > log.txt
-type log.txt
-assembler\fixheadr Dr_Robotnik's_Mean_Bean_Machine.md
